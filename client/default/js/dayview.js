ba8751d0cb29e0e66f432c3645955d31188cf2f6
Michael Monte <michaeljmonte@gmail.com>
2011-10-10 13:56:05 -0400

    Pulled javascript into its own file.

---
+++ client/default/js/dayview.js
@@ -0,0 +1,192 @@
+/**
+ * JavaScript Code for day view.
+ */
+
+function roundNumber(num, dec) {
+	var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
+	return result;
+}
+
+function setDate(d) {
+	var curr_date = d.getDate();
+	var curr_month = d.getMonth();
+	$("#WorkWeekCalendar").data("currdate", d);
+	curr_month++;
+	var curr_year = d.getFullYear();
+	$("#fkDate").val(curr_year + "-" + curr_month + "=" + curr_date);
+	if (curr_date.toString().length == 1)
+		curr_date = "0" + curr_date.toString();
+	if (curr_month.toString().length == 1)
+		curr_month = "0" + curr_month.toString();
+	$("#HeaderDate").text(curr_month + "/" + curr_date + "/" + curr_year);
+
+}
+function goDay(date) {
+	$.mobile.showPageLoadingMsg();
+	setDate(date);
+	$("#entryList-contain").html("");
+	var adjusteddate = new Date(date);
+	adjusteddate.setDate(adjusteddate.getDate() - 1);
+	$fh
+			.act(
+					{
+						act : 'getDate',
+						secure : true,
+						req : {
+							date : adjusteddate.getTime()
+						}
+					},
+					function(res) {
+						var winheight;
+						$fh.env({/* none */}, function(envprops) {
+							winheight = envprops.height;
+						}, function(code, errorprops, params) { /* none */
+						});
+
+						var listheight = winheight - 90;
+						var hourHeight = listheight / 24;
+						var d = new Date(res.date);
+						d.setDate(d.getDate() + 1);
+						setDate(d);
+						$("#entryList-contain")
+								.html(
+										'<ul data-role="listview" id="entryList" style="width:85%;float:right;margin-right:1px;margin-top:0;"></ul>')
+								.css("height", listheight + "px").css(
+										"background-size",
+										"auto " + listheight + "px");
+						var float = "right";
+						for ( var i in res.timecards) {
+
+							var starton = new Date(res.timecards[i].starton);
+							var endon = new Date(res.timecards[i].endon);
+
+							var one_hour = 1000 * 60 * 60;
+							var TimeSpan = roundNumber(
+									(endon.getTime() - starton.getTime())
+											/ (one_hour), 2)
+							var prevEnd;
+
+							prevEnd = new Date(starton);
+							prevEnd.setHours(0);
+							prevEnd.setMinutes(0);
+
+							var margintop = roundNumber(
+									((starton.getTime() - prevEnd.getTime()) / one_hour),
+									2)
+									* hourHeight;
+
+							var li = $("<li />");
+							li
+									.append(
+											$("<a />")
+													.attr("href", "#")
+													.css("paddingTop", "0")
+													.append(
+															$("<h6 />")
+																	.text(
+																			res.timecards[i].chargenumber)
+																	.css(
+																			"marginTop",
+																			"0"))
+													.append(
+															$("<span>")
+																	.addClass(
+																			"ui-li-aside")
+																	.text(
+																			TimeSpan
+																					+ " hours")
+																	.css(
+																			"marginTop",
+																			"0"))
+													.append(
+															$("<p />")
+																	.text(
+																			res.timecards[i].comments)
+																	.css(
+																			"border",
+																			"1px solid #ddd")
+																	.css(
+																			"border-width",
+																			"1px 0 0 0")
+																	.css(
+																			"overflow",
+																			"hidden")
+																	.css(
+																			"margin-top",
+																			"-5px")
+																	.css(
+																			"padding-top",
+																			"4px"))
+
+									).css(
+											"height",
+											Math.round(hourHeight * TimeSpan)
+													+ "px").css("top",
+											Math.round(margintop + 43) + "px")
+									.css("position", "absolute").data("id",
+											res.timecards[i].id)
+									.click(function() {
+
+										loadTimecard($(this).data("id"));
+
+									});
+
+							if (float == "right") {
+								float = "left";
+								li.css("left", "10%");
+							} else {
+								float = "right";
+								li.css("right", "00");
+							}
+
+							if ((res.timecards[(parseInt(i) - 1)] && res.timecards[(parseInt(i) - 1)].endon > res.timecards[i].starton)
+									|| (res.timecards[(parseInt(i) + 1)] && res.timecards[(parseInt(i) + 1)].starton < res.timecards[i].endon)) {
+								li.css("width", "45%");
+							} else {
+								li.css("width", "90%");
+							}
+							$("#entryList").append(li);
+						}
+						$("#entryList").listview();
+						$.mobile.hidePageLoadingMsg();
+					});
+}
+$('#WorkWeekCalendar').live('pagecreate', function(event) {
+	goDay(new Date());
+	$('#WorkWeekCalendar').live('swiperight', function(event) {
+		var d = $("#WorkWeekCalendar").data("currdate");
+		d.setDate(d.getDate() - 1);
+		goDay(d);
+	});
+	$('#WorkWeekCalendar').live('swipeleft', function(event) {
+		var d = $("#WorkWeekCalendar").data("currdate");
+		d.setDate(d.getDate() + 1);
+		goDay(d);
+	});
+
+	$("#btnDayPrev").click(function() {
+		var d = $("#WorkWeekCalendar").data("currdate");
+		d.setDate(d.getDate() - 1);
+		goDay(d);
+	});
+	$("#btnDayNext").click(function() {
+		var d = $("#WorkWeekCalendar").data("currdate");
+		d.setDate(d.getDate() + 1);
+		goDay(d);
+	});
+
+	$("#HeaderDate").click(function() {
+		$("#fkDate").datebox("open");
+	});
+
+	$('#fkDate').bind('datebox', function(e, p) {
+		if (p.method === 'set') {
+
+			$('#fkDate').datebox('close');
+			var d = new Date(p.value);
+			d.setDate(d.getDate() + 1);
+			goDay(d);
+		}
+	});
+
+});
---
+++ client/default/js/timecardview.js
@@ -0,0 +1,181 @@
+/**
+ * Timecard view.
+ */
+
+$("#TimecardDetail").live(
+		'pagecreate',
+		function() {
+			$("#btnCloseTimecard").click(function() {
+				$.mobile.changePage($("#WorkWeekCalendar"), {
+					reverse : true
+				});
+			});
+			$("#btnNewTimecard").click(function() {
+				loadTimecard(new Date($("#fkDate").val()));
+			});
+
+			$("#btnSaveTimecard").click(
+					function() {
+						$.mobile.showPageLoadingMsg();
+						var starton = new Date($("#TimecardFullDate").val()
+								+ " " + $("#txtStartOn").val());
+						var endon = new Date($("#TimecardFullDate").val() + " "
+								+ $("#txtEndOn").val());
+						// alert($("#TimecardFullDate").val());
+						if ($("#txtStartOn").val() == "")
+							starton = "";
+						if ($("#txtEndOn").val() == "")
+							endon = "";
+
+						var comments = $("#txtComments").text();
+						if (comments == "")
+							comments = $("#txtComments").val()
+
+						var req = {
+							"id" : $("#txtId").val(),
+							"starton" : starton.getTime(),
+							"endon" : endon.getTime(),
+							"comments" : comments,
+							"chargenumber" : $("#txtChargeNumber").val(),
+							"reason" : $("#txtChargeNumber").val()
+						};
+						$fh.act({
+							act : 'saveTimecard',
+							secure : true,
+							req : req
+						}, function(res) {
+							if (res.success)
+								$.mobile.changePage($("#WorkWeekCalendar"), {
+									reverse : true
+								});
+							else {
+								$("#errorList").html("");
+
+								for ( var i in res.messages) {
+									$("<li>" + res.messages[i] + "</li>")
+											.appendTo($("#errorList"));
+								}
+								$.mobile.changePage($("#Validation"), {
+									transition : "pop"
+								});
+							}
+							$.mobile.hidePageLoadingMsg();
+						});
+					});
+		});
+
+function loadTimecard(id) {
+	$.mobile.changePage($("#TimecardDetail"));
+	$.mobile.showPageLoadingMsg();
+	var winheight;
+	$fh.env({/* none */}, function(envprops) {
+		winheight = envprops.height;
+	}, function(code, errorprops, params) { /* none */
+	});
+
+	var listheight = winheight - 90;
+	$("#TimeCardForm").css("height", listheight + "px").css("overflow", "auto");
+
+	if (!id.getHours) {
+		$("#txtId").val(id);
+		$fh.act({
+			act : 'getTimecard',
+			secure : true,
+			req : {
+				id : id
+			}
+		}, function(res) {
+			var starton = new Date(res.timecard.starton);
+			// alert(starton);
+			$("#TimecardFullDate").val(
+					(starton.getMonth() + 1) + "/" + starton.getDate() + "/"
+							+ starton.getFullYear());
+			// alert($("#TimecardFullDate").val());
+			$("#txtStartOn").val(formatTime(new Date(res.timecard.starton)));
+			$("#txtEndOn").val(formatTime(new Date(res.timecard.endon)));
+			$("#txtComments").text(res.timecard.comments);
+
+			$("#txtChargeNumber").html("").append(
+					$('<option value=""></option>'));
+			for ( var i in res.chargenumbers) {
+				var opt = $("<option />");
+				opt.val(res.chargenumbers[i].id)
+						.text(res.chargenumbers[i].name);
+				$("#txtChargeNumber").append(opt);
+			}
+
+			$("#txtChargeNumber").val(res.timecard.chargenumber);
+			$("#txtChargeNumber").selectmenu('refresh', true);
+
+			$("#txtReason").html("").append($('<option value=""></option>'));
+			for ( var i in res.reasons) {
+				var opt = $("<option />");
+				opt.val(res.reasons[i].id).text(res.reasons[i].name);
+				$("#txtReason").append(opt);
+			}
+
+			$("#txtReason").val(res.timecard.reason);
+			$("#txtReason").selectmenu('refresh', true);
+
+			$("#mileageList").live('click', function() {
+				loadMileage($("#txtId").val());
+			});
+			$.mobile.hidePageLoadingMsg();
+		});
+	} else {
+		$("#TimecardFullDate").val(
+				(id.getMonth() + 1) + "/" + id.getDate() + "/"
+						+ id.getFullYear())
+		$fh.act({
+			act : 'newTimecard',
+			secure : true,
+			req : {
+				date : id.getTime()
+			}
+		}, function(res) {
+			$("#txtChargeNumber").html("").append(
+					$('<option value=""></option>'));
+			for ( var i in res.chargenumbers) {
+				var opt = $("<option />");
+				opt.val(res.chargenumbers[i].id)
+						.text(res.chargenumbers[i].name);
+				$("#txtChargeNumber").append(opt);
+			}
+			$("#txtChargeNumber").selectmenu('refresh', true);
+
+			$("#txtReason").html("").append($('<option value=""></option>'));
+			for ( var i in res.reasons) {
+				var opt = $("<option />");
+				opt.val(res.reasons[i].id).text(res.reasons[i].name);
+				$("#txtReason").append(opt);
+			}
+			$("#txtReason").selectmenu('refresh', true);
+
+			$("#txtId").val(res.guid);
+
+			$("#txtStartOn").val("");
+			$("#txtEndOn").val("");
+			$("#txtComments").text("");
+			$.mobile.hidePageLoadingMsg();
+		});
+	}
+}
+
+function formatTime(time) {
+	var ret = "";
+	if (time.getHours() > 12) {
+		ret += (time.getHours() - 12) + ":";
+	} else {
+		ret += (time.getHours()) + ":";
+	}
+
+	if (time.getMinutes() < 10)
+		ret += "0";
+	ret += time.getMinutes();
+
+	if (time.getHours() >= 12)
+		ret += " PM";
+	else
+		ret += " AM";
+	return ret;
+}
