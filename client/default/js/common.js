e5ed49493fde343fb03fd915e8578901be7a5501
cbjennings <clark.jennings@capricciosoftware.com>
2011-10-25 16:38:33 -0400

    debug

--- client/default/js/common.js
+++ client/default/js/common.js
@@ -13,6 +13,7 @@ function CallService(ServiceName, Data, Success, Error) {
 		url:"http://localhost:59703/WebServices/MobileService.asmx/"+ServiceName,
 		//jsonpCallback:"fnSuccess",
 		success:function(d) {
+			alert(d);
 			var data=JSON.parse(d.d);
 			if(data.success===false && Error==="DisplayMessages") {
 				//Common error/validation message popup
