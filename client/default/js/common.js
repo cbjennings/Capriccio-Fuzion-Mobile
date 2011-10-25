293452eee1bd9acae6f9b1636d65d967f0c7802a
cbjennings <clark.jennings@capricciosoftware.com>
2011-10-25 16:40:57 -0400

    debug

--- client/default/js/common.js
+++ client/default/js/common.js
@@ -5,16 +5,15 @@ function CallService(ServiceName, Data, Success, Error) {
 
 	
 	$.ajax({
-		type:"POST",
-		contentType: "application/json; charset=utf-8",
-		data:JSON.stringify(Data),
+		
+		data:Data,
 		dataType:"jsonp",
 		//url:"https://www.capricciofuzion.com/"+$("#txtCompany").val()+"/web/webservices/MobileService.asmx/"+ServiceName+"?jsoncallback=?",
 		url:"http://localhost:59703/WebServices/MobileService.asmx/"+ServiceName,
 		//jsonpCallback:"fnSuccess",
 		success:function(d) {
-			alert(d);
-			var data=JSON.parse(d.d);
+			alert(d.d);
+			/*var data=JSON.parse(d.d);
 			if(data.success===false && Error==="DisplayMessages") {
 				//Common error/validation message popup
 				$("#errorList").html("");
@@ -33,7 +32,7 @@ function CallService(ServiceName, Data, Success, Error) {
 			} else {
 				Success(data);
 			}
-			return false;
+			return false;*/
 		},
 		error:function(jqXHR, textStatus, errorThrown) {
 			alert(JSON.stringify({'XHR':jqXHR,'status':textStatus,'error':errorThrown}))
