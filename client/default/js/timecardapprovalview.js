9cc44b51381b615bcce5f013817266a2c5602a78
cbjennings <clark.jennings@capricciosoftware.com>
2011-10-26 15:34:57 -0400

    remove X button from dialog header

--- client/default/js/timecardapprovalview.js
+++ client/default/js/timecardapprovalview.js
@@ -41,8 +41,10 @@ $("#TimecardApprovalDetail").live(
 			$("#btnSubmitTimecardApproval").click(function() {
 				//alert('submit approval clicked');
 				$.mobile.changePage($("#TimecardApprovalDialog"), {
-					transition : "pop"
+					transition : "pop",
+					
 				});
+				$("#TimecardApprovalDialog .ui-header a:jqmData(icon='delete')").remove();
 			});
 
 			$("#btnConfirmYes").click(
