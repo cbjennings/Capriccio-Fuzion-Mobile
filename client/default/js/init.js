82b7b4c1442d7507fc1e2ac26de9aadbf07f59b4
cbjennings <clark.jennings@capricciosoftware.com>
2011-10-27 18:02:49 -0400

    init confusion

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -1,56 +1,71 @@
 //Ready Function
 $(function() {
-	
-	$.mobile.changePage($("#Login"));
-  $.mobile.selectmenu.prototype.options.nativeMenu = false;
-   var user, pass, company;
-  $fh.data({key:'username'}, function(res) {
-    user=res.val; 
-    $("#txtUsername").val(user);
-  });
-  $fh.data({key:'password'}, function(res) {
-    pass=res.val; 
-    $("#txtPassword").val(pass);
-    if(pass) $("#chkStorePassword").attr("checked","checked");
-  });
-  $fh.data({key:'company'}, function(res) {
-    company=res.val; 
-    $("#txtCompany").val(company);
-  });
-    
 
-  $("#btnLogin").click(function() {
-    if(
-      $("#txtUsername").val() && 
-      $("#txtPassword").val() && 
-      $("#txtCompany").val()
-	) {
-      
-      
-      CallService(
-    	'Login',
-		{
-			username:JSON.stringify($("#txtUsername").val()),
-			password:JSON.stringify($("#txtPassword").val())
-		},
-		function(res) {
-      	  $fh.data({act:'save', key:'username', val:$("#txtUsername").val()});
-            $fh.data({act:'save', key:'company', val:$("#txtCompany").val()});
-            if($("#chkStorePassword").attr("checked")) {
-              $fh.data({act:'save', key:'password', val:$("#txtPassword").val()}); 
-            } else {
-              $fh.data({act:'save', key:'password', val:''});
-            }
-            sessionId = res.sessionId
-            $.mobile.hidePageLoadingMsg();
-            goDay(new Date());
-            //$.mobile.changePage($("#DayViewCalendar"));  
-        },
-        "DisplayMessages"
-	  );
-    }
-  });
-  if(user&&pass&&company) $("#btnLogin").click(); 
-  
-  $.mobile.fixedToolbars.setTouchToggleEnabled(false)
+	$.mobile.selectmenu.prototype.options.nativeMenu = false;
+	var user, pass, company;
+	$fh.data({
+		key : 'username'
+	}, function(res) {
+		user = res.val;
+		$("#txtUsername").val(user);
+	});
+	$fh.data({
+		key : 'password'
+	}, function(res) {
+		pass = res.val;
+		$("#txtPassword").val(pass);
+		if (pass)
+			$("#chkStorePassword").attr("checked", "checked");
+	});
+	$fh.data({
+		key : 'company'
+	}, function(res) {
+		company = res.val;
+		$("#txtCompany").val(company);
+	});
+
+	$("#btnLogin").click(
+			function() {
+				if ($("#txtUsername").val() && $("#txtPassword").val()
+						&& $("#txtCompany").val()) {
+
+					$.mobile.changePage($("#Login"));
+					CallService('Login', {
+						username : JSON.stringify($("#txtUsername").val()),
+						password : JSON.stringify($("#txtPassword").val())
+					}, function(res) {
+						$fh.data({
+							act : 'save',
+							key : 'username',
+							val : $("#txtUsername").val()
+						});
+						$fh.data({
+							act : 'save',
+							key : 'company',
+							val : $("#txtCompany").val()
+						});
+						if ($("#chkStorePassword").attr("checked")) {
+							$fh.data({
+								act : 'save',
+								key : 'password',
+								val : $("#txtPassword").val()
+							});
+						} else {
+							$fh.data({
+								act : 'save',
+								key : 'password',
+								val : ''
+							});
+						}
+						sessionId = res.sessionId
+						$.mobile.hidePageLoadingMsg();
+						goDay(new Date());
+						// $.mobile.changePage($("#DayViewCalendar"));
+					}, "DisplayMessages");
+				}
+			});
+	if (user && pass && company)
+		$("#btnLogin").click();
+
+	$.mobile.fixedToolbars.setTouchToggleEnabled(false)
 });
