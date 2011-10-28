6af1000de7fd5b7f3af16f6d81c07bfa8a8507ed
cbjennings <clark.jennings@capricciosoftware.com>
2011-10-28 12:51:44 -0400

    debug

--- client/default/js/timecardview.js
+++ client/default/js/timecardview.js
@@ -34,9 +34,9 @@ $("#TimecardDetail").live(
 								//"chargenumber" : JSON.stringify($("#txtChargeNumber").val()),
 								//"reason" : JSON.stringify($("#txtReason").val())
 						};
-						if(req.id===JSON.stringify("")) req.id=JSON.stringify("{00000000-0000-0000-0000-000000000000}");
-						if(req.chargenumber===JSON.stringify(""))req.chargenumber=JSON.stringify("{00000000-0000-0000-0000-000000000000}");
-						if(req.reason===JSON.stringify(""))req.reason=JSON.stringify("{00000000-0000-0000-0000-000000000000}");
+						//if(req.id===JSON.stringify("")) req.id=JSON.stringify("{00000000-0000-0000-0000-000000000000}");
+						//if(req.chargenumber===JSON.stringify(""))req.chargenumber=JSON.stringify("{00000000-0000-0000-0000-000000000000}");
+						//if(req.reason===JSON.stringify(""))req.reason=JSON.stringify("{00000000-0000-0000-0000-000000000000}");
 						CallService( 
 								'saveTimecard', 
 								req, 
